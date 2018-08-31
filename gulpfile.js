2f8175599ae4e5d96ea2a5490b935c11ec582fba
Rosen Ivanov <rosenplamenov93@gmail.com>
2018-08-31 09:49:49 +0100

    Nasty

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,35 @@
+'use strict';
+
 var gulp = require('gulp');
-var pug = require('gulp-pug');
-var less = require('gulp-less');
-var minifyCSS = require('gulp-csso');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+let cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
-gulp.task('html', function () {
-    return gulp.src('client/templates/*.pug')
-        .pipe(pug())
-        .pipe(gulp.dest('build/html'))
+gulp.task('image', () =>
+    gulp.src('images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'))
+);
+
+gulp.task('sass', function () {
+    return gulp.src('*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
 });
 
-gulp.task('css', function () {
-    return gulp.src('client/templates/*.less')
-        .pipe(less())
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('build/css'))
+gulp.task('minify-css', () => {
+    return gulp.src('css/*.css')
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('js', function () {
-    return gulp.src('client/javascript/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(concat('app.min.js'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/js'))
+//Watch tasks
+gulp.task('watch', function () {
+    gulp.watch('*.scss', ['sass']);
+    gulp.watch('css/*.css', ['minify-css']);
+    gulp.watch('images/*', ['image']);
 });
 
-gulp.task('default', ['html', 'css', 'js']);
+gulp.task('default', ['minify-css', 'sass', 'image']);
